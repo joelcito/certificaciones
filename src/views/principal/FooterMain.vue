3b3a327d046fe7e8bb2dcf6207ca529513ac9a9f
krlos.pacha <pckrlos@gmail.com>
2024-05-13 17:01:27 -0400

    added footer file

--- src/views/principal/FooterMain.vue
+++ src/views/principal/FooterMain.vue
@@ -4,14 +4,16 @@
       <footer class="footer py-4 bg-dark text-light"> 
           <div class="container text-center">
               <!-- <p class="mb-4 small">Copyright <script>document.write(new Date().getFullYear())</script> &copy; <a href="http://www.devcrud.com">DevCRUD</a></p> -->
-              <div class="social-links">
-                  <a href="#" class="link"><i class="ti-facebook">face</i></a>
-                  <a href="#" class="link"><i class="ti-twitter-alt"></i>twitter</a>
-                  <a href="#" class="link"><i class="ti-google"></i>google</a>
-                  <a href="#" class="link"><i class="ti-pinterest-alt"></i></a>
-                  <a href="#" class="link"><i class="ti-instagram"></i></a>
-                  <a href="#" class="link"><i class="ti-rss"></i></a>
-              </div>
+              <v-btn
+                    v-for="icon in icons"
+                    :key="icon"
+                    :icon="icon"
+                    class="sx-1"
+                    variant="text"
+                ></v-btn>
+            
+
+        Servicio general de identificación Personal © 2024 | v1.0.0
           </div>
       </footer>
   
@@ -22,8 +24,14 @@
    
   import {RouterLink} from 'vue-router'
   
-  import { ref } from 'vue';
+  import { ref,reactive } from 'vue';
 
+  const icons = reactive( [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ])
 
   
   const Regform = ref();
