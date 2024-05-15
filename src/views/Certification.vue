e22b841e33149c5e32e88fdea852a70a308e046c
krlos.pacha <pckrlos@gmail.com>
2024-05-15 17:48:20 -0400

    added validation to go the next on the main form

--- src/views/Certification.vue
+++ src/views/Certification.vue
@@ -18,10 +18,13 @@
           <v-col col="12" lg="12" md="12" sm="12">
             
               <v-container class="sameheight">
+
                 <v-stepper 
                 class="custom" 
                 alt-labels
                   :items="timeLineMenu"
+                  hide-actions
+                  v-model="step"
                 >
                 
                   <template v-slot:item.1>
@@ -46,8 +49,8 @@
                               ></v-tab>
                             </template>
 
-                            <template v-slot:item="{ item }">
-                              <v-tabs-window-item :value="item.value" class="pa-4">
+                            <template v-slot:item="{ item }" >
+                              <v-tabs-window-item :value="item.value" class="pa-8">
                                 <ol>
                                   <li v-for="term in item.content">
                                     {{ term.term }}
@@ -57,6 +60,24 @@
                             </template>
                           
                           </v-tabs>
+
+                          <v-form v-model="isFormValid">
+                              <v-card-text>
+                                <!-- <v-checkbox v-model="swpoliticas" :rules="politicasRules"  label="Aceptar politicas?"></v-checkbox> -->
+
+                                <v-checkbox
+                                  :true-value="1"
+                                  :false-value="0"
+                                  :rules=politicasRules
+                                  v-model="swpoliticas"
+                                  />                                
+                              </v-card-text>
+
+
+                          </v-form>  
+
+
+                                                  
                         </v-sheet>
             
                       </v-card>
@@ -68,7 +89,14 @@
                     <v-card title="step 2 " color="#F8983D" flat class="rounded-card">
                       <v-sheet color="#fff" elevation="3" rounded="lg">
                         <div >
-                          <formInfoPerson />                        
+                          <formInfoPerson />    
+                          <v-form v-model="isFormValid">
+                            <v-card-text>
+                              <v-text-field  v-model="email" :rules="emailRules" :counter="50"
+                                  label="Email" minlength="5"  maxlength="50"  required
+                              ></v-text-field>
+                            </v-card-text>
+                          </v-form>                                              
                         </div>
                       </v-sheet>
                     </v-card>
@@ -81,8 +109,21 @@
                       </v-sheet>
                     </v-card>
                   </template>
-                
+              
                 </v-stepper>
+
+                <v-card-actions>
+                    <!-- <v-btn  text @click="step--"> -->
+                    <v-btn  text @click="nextstep('prev',step)">
+                      Atras
+                    </v-btn>
+                    <v-spacer></v-spacer>
+                    <!-- <v-btn  color="primary" depressed @click="step++"> -->
+                      
+                    <v-btn :disabled="!isFormValid" color="primary"  @click="nextstep('con',step)">
+                      Siguiente
+                    </v-btn>
+                  </v-card-actions>                  
 
               </v-container>
             
@@ -104,7 +145,7 @@ import FooterMain from './principal/FooterMain.vue'
 import timeLineMenu from '../data/timeLineMenu.json'
 import {RouterLink} from 'vue-router'
 import { createVuetify } from 'vuetify'
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed, createApp } from 'vue';
 
 import imageSegip from "../assets/compu.png"
 import ciMain from "../assets/cimain.png"
@@ -112,73 +153,100 @@ import formInfoPerson from './formInfoPerson.vue'
 import DataVerification from './DataVerification.vue'
 
 
+
+
+const step = ref(1)
+let isFormValid = ref(false)
+
+
+let swpoliticas = ref(false)
+const politicasRules = reactive([v => !!v || 'You must agree to continue!'])
+
+
+const email= ref("")
+const emailRules = reactive( [
+    v => !!v || "Email is required",
+    (v) =>
+    v.length >= 5 ||
+    "The e-mail address must contain at least 5 characters",
+    (v) =>
+    v.length <= 50 ||
+    "The e-mail address cannot be longer than 50 characters",
+    v => /.+@.+/.test(v) || "Please enter a valid email address"]
+    )
+
+
+
+
 const tab = ref('tab-1')
-  const tabs = reactive([
-        {
-          icon: 'mdi-book-open-page-variant',
-          text: 'ACEPTACIÓN DE LOS TERMINOS',
-          value: 'tab-1',
-          content: [
-            {
-              id:1,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-            {
-              id:2,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-            {
-              id:3,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-          ]
-        },
-        {
-          icon: 'mdi-handshake-outline',
-          text: 'RESPONSABILIDADES',
-          value: 'tab-2',
-          content: [
-            {
-              id:1,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-            {
-              id:2,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-            {
-              id:3,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-          ]
-        },
-        {
-          icon: 'mdi-license',
-          text: 'DECLARACIÓN',
-          value: 'tab-3',
-          content: [
-            {
-              id:1,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-            {
-              id:2,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-            {
-              id:3,
-              term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-            },
-          ]
-        },
-      ]
-      )
-  const icons = reactive( [
-      'mdi-facebook',
-      'mdi-twitter',
-      'mdi-linkedin',
-      'mdi-instagram',
-    ])
+const tabs = reactive([
+
+
+      {
+        icon: 'mdi-book-open-page-variant',
+        text: 'ACEPTACIÓN DE LOS TERMINOS',
+        value: 'tab-1',
+        content: [
+          {
+            id:1,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+          {
+            id:2,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+          {
+            id:3,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+        ]
+      },
+      {
+        icon: 'mdi-handshake-outline',
+        text: 'RESPONSABILIDADES',
+        value: 'tab-2',
+        content: [
+          {
+            id:1,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+          {
+            id:2,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+          {
+            id:3,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+        ]
+      },
+      {
+        icon: 'mdi-license',
+        text: 'DECLARACIÓN',
+        value: 'tab-3',
+        content: [
+          {
+            id:1,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+          {
+            id:2,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+          {
+            id:3,
+            term:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+          },
+        ]
+      },
+    ]
+    )
+const icons = reactive( [
+    'mdi-facebook',
+    'mdi-twitter',
+    'mdi-linkedin',
+    'mdi-instagram',
+  ])
 
 const Regform = ref();
 
@@ -187,6 +255,25 @@ function validate() {
   return 1
   //Regform.value.validate();
 }
+function nextstep(opt, num){
+  isFormValid.value = false
+  console.log('disabled ',isFormValid.value)  
+
+  if(opt == 'prev'){
+    step.value = step.value - 1
+  }
+  if(opt == 'con'){
+    step.value = step.value + 1
+  }
+  
+}
+
+/*function valPoliticas(){
+  isFormValid.value = !!swpoliticas.value    
+  console.log('change method', isFormValid.value)
+  return;
+}*/
+
 </script>
 
 <style lang="css">
@@ -246,7 +333,7 @@ background-color: #fff;  /* Añade el color que prefieras */
 }
 
 .custom  {
-background-color: #4b768a;  /* Añade el color que prefieras */
+background-color: #fff;  /* Añade el color que prefieras 4b768a */
 }
 
 .v-stepper-actions  {
