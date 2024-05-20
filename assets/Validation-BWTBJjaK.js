8d84dfaba327332c0ae11eb903a3ec929c0cfa15
JOEL FLORES QUISPE <joel.flores@segip.gob.bo>
2024-05-20 19:32:28 -0400

    incial deploy

---
+++ assets/Validation-BWTBJjaK.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-Bt93H8To.js";const n={};function r(a,c){return" validation "}const o=t(n,[["render",r]]);export{o as default};
---
+++ config.js
@@ -0,0 +1,3 @@
+window.__APP_CONFIG__ = {
+    VITE_API_BASE_URL_JAVA_BACKEND: 'http://localhost:9090',
+};
